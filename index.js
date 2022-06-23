a166e71a8dab7baf688e6cd726ae64cf05064913
Leobolo <49465127+Leobolo@users.noreply.github.com>
2022-06-23 23:44:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-const statement = "My name is Ajayi Oluwasegun. <br>My height is 1.65Cm. <br>My country is Nigeria.";
+const statement = "My name is Ajayi Oluwasegun. <br>My height is 1.79m. <br>My country is Nigeria.";
 document.write(statement);
